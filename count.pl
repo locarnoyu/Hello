e47b5d9df8917567c85b5bf8173a4394165b6468
wyu <wyu@opentext.com>
2015-02-26 16:39:20 -0500

    update to cover full folder and only print 50+ ones

--- count.pl
+++ count.pl
@@ -1,35 +1,51 @@
     use strict;
     use warnings;
-     
-    my %count;
-    my $file = shift or die "Usage: $0 FILE\n";
+	use Cwd;
 	
-	my $directory = '.';
+	
+    my %count;
+	my $directory;
 
-    opendir (DIR, $directory) or die $!;
-	while (my $file = readdir(DIR)) {
+	my $root = "c:\\wyu\\personal\\research";
 
-        print "$file\n";
-		if ($file eq "." or $file eq "..")
-		{
-		    next;
+    opendir my $dh, $root or die "$0: opendir: $!";
+
+    while (defined(my $directory = readdir $dh)) {
+        next unless -d "$root/$directory";
+		print "path = $root/$directory \n";
+		next if ($directory eq "." | $directory eq ".." | $directory eq ".git" | $directory eq "term_frequency_results");
+		my $sub_folder = $root. "\/" . $directory ;
+		print "sub = $sub_folder \n";
+		chdir $sub_folder;
+		#system ("cd $sub_folder");
+		my $cwd = getcwd();
+	    print "$cwd \n";
+		opendir (DIR, ".") or die $!;
+		while (my $file = readdir(DIR)) {
+			if ($file eq "." or $file eq "..")
+			{
+				next;
+			}
+
+			open my $fh, '<', $file or die "Could not open '$file' $!";
+			while (my $line = <$fh>) {
+				chomp $line;
+				$line =~ s/[\|\;\:\}\{\<\>\"\?\(\)\=\/\-\.\&\^\$\#\@\!\~\_\,\.\/\[\]\'\\\%\*\+]/ /g;
+				foreach my $str (split /\s+/, $line) {
+					$count{$str}++;
+				}
+			}	
 		}
+		chdir $root;
+		#system ("cd ..");
+	}
 
-		open my $fh, '<', $file or die "Could not open '$file' $!";
-        while (my $line = <$fh>) {
-            chomp $line;
-	        $line =~ s/[\|\;\:\}\{\<\>\"\?\(\)\=\/\-\.\&\^\$\#\@\!\~\_\,\.\/\[\]\'\\\%]/ /g;
-            foreach my $str (split /\s+/, $line) {
-                $count{$str}++;
-            }
-		}	
-    }
-
-    
+   
 	#exit 0;
     foreach my $name (sort { $count{$a} <=> $count{$b} } keys %count) {
 	  if (length($name) > 2){
-         printf "%-8s %s\n", $name, $count{$name};
+	     my $total = $count{$name} ;
+		 printf "%-8s %s\n", $name, $total if ($total > 50);
 	  }
     }
 	
